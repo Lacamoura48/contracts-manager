391c591afbc95e2e9da915c2d89bf039efa6d16a
Lacamoura48 <anasfog@outlook.com>
2024-10-06 16:36:19 +0100

    added github action

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use App\Http\Controllers\ClientController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
