95c0c427ff98d0b3d33c8ae1da9ae28eeaa06c09
Lacamoura48 <anasfog@outlook.com>
2024-10-26 15:41:24 +0100

    k

--- resources/js/Components/lists/BondsList.jsx
+++ resources/js/Components/lists/BondsList.jsx
@@ -1,14 +1,9 @@
 import BondLine from '../cards/BondLine';
 
 function BondsList({ bonds }) {
-    let foundCurrent = false;
     return (
         <div className="mt-8 w-full">
             {bonds.map((line) => {
-                const currentOne = !foundCurrent && line.status !== 'paid';
-                if (currentOne) {
-                    foundCurrent = true;
-                }
                 return <BondLine key={line.id} bond={line} />;
             })}
         </div>
