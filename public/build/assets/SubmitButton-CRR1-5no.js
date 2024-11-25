81320093cd56274ac91adc78a6b4297679afa341
Lacamoura48 <anasfog@outlook.com>
2024-11-25 17:46:37 +0100

    build

--- public/build/assets/SubmitButton-CRR1-5no.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./app-DP3tp7Wp.js";import{L as o}from"./loader-circle-Bjxkxk69.js";function i({children:s,loading:t}){return e.jsx("button",{disabled:t,className:`${t&&"opacity-40"} text-bold block w-full rounded-lg bg-black px-5 py-3 text-white`,children:t?e.jsx("span",{className:"mx-auto block w-fit animate-spin",children:e.jsx(o,{})}):s})}export{i as S};
