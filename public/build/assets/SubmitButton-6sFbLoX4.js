0282537c30986a620c1076807e83f7700df0e86b
Lacamoura48 <anasfog@outlook.com>
2024-12-01 12:17:50 +0100

    push build trash

---
+++ public/build/assets/SubmitButton-6sFbLoX4.js
@@ -0,0 +1,1 @@
+import{j as e}from"./app-1Xwr3j2F.js";import{L as o}from"./loader-circle-n8Q3zkey.js";function i({children:s,loading:t}){return e.jsx("button",{disabled:t,className:`${t&&"opacity-40"} text-bold block w-full rounded-lg bg-black px-5 py-3 text-white`,children:t?e.jsx("span",{className:"mx-auto block w-fit animate-spin",children:e.jsx(o,{})}):s})}export{i as S};
